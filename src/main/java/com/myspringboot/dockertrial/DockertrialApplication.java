340dbe81bd1ac12e4fbb96b5d45e0416bc25007c
mariam george <mariam0107@gmail.com>
2025-04-21 20:39:02 +0200

    Created Docker test demo app

---
+++ src/main/java/com/myspringboot/dockertrial/DockertrialApplication.java
@@ -0,0 +1,13 @@
+package com.myspringboot.dockertrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockertrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockertrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myspringboot/dockertrial/DockertrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringboot.dockertrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockertrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
